a98be23a11567a73ca43069cceaa4c845f03e36d
baridoo saro <sarobaridoo@gmail.com>
2024-10-31 03:04:38 -0700

    getting a trailer

--- src/Requests/Row.js
+++ src/Requests/Row.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import instance from './URL';
 import './row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_Url = "https://image.tmdb.org/t/p/original/";
 
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     async function fetchData() {
       const request = await instance.get(fetchUrl);
@@ -17,18 +20,37 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts ={
+    height: "400",
+    width: "100",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+        const UrlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(UrlParams.get('v'));
+      })
+      .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className='movierow'>
       <h2>{title}</h2>
       <div className="row_poster">
         {/*several row posters*/}
         {movies.map(movie => (
-          <img key={movie.id} src={`${base_Url}${isLargeRow ? movie.poster_path : movie.poster_path}`} alt={movie.name} />
+          <img key={movie.id} onClick={() => handleClick(movie)} src={`${base_Url}${isLargeRow ? movie.poster_path : movie.poster_path}`} alt={movie.name} />
         ))};
       </div>
-      <div className="rating">
-        <p>Rating:{movies.map(movie => (movie.vote_average))}</p>
-      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
