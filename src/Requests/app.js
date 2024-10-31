95d8e630253a13bbf7eb59f97f86d32ca189bebb
Saro <sarobaridoo@gmail.com>
2024-10-31 15:43:27 +0100

    handle routing

--- src/Requests/app.js
+++ src/Requests/app.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Row from './Row';
-import  Row from './Row';
+import MovieDetail from './MovieDetail';
 
 function App() {
   return (
