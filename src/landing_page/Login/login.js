b593ac1c7c567275bfc6036180d8d5ef2415d310
baridoo saro <sarobaridoo@gmail.com>
2024-11-05 01:06:38 -0800

    worked on the navigation

--- src/landing_page/Login/login.js
+++ src/landing_page/Login/login.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
+import Movies from '../../Homepage/Movies/movie.js';
 
 
 const login = () => {
-  
-  return (
-    <div className='form'>
-      <h2>Login</h2>
-      <p>Login to your account</p>
-      <form action="">
-        <div className='dt'>
-          <label for="Username">Username</label>
-          <input type="text" name='Email' placeholder='Email' id='email' required />
-        </div>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-        <div className='dt'>
-          <label htmlFor="Password">Password</label>
-          <input type="password" name='password' placeholder='Password' id='password' required />
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div>
+      { isLoggedIn ? (<Movies />) :
+      (
+        <div className='form'>
+          <h2>Login</h2>
+          <p>Login to your account</p>
+          <form action="">
+            <div className='dt'>
+              <label for="Username">Username</label>
+              <input type="text" name='Email' placeholder='Email' id='email' required />
+            </div>
+
+            <div className='dt'>
+              <label htmlFor="Password">Password</label>
+              <input type="password" name='password' placeholder='Password' id='password' required />
+            </div>
+            <button type='submit' onClick={() => ({handleLogin})}>Log In</button>
+            <p className='footnote'>By clicking you have agreed to our terms and<br /> conditions</p>
+          </form>
+        </div >
+        )}
         </div>
-        <button type='submit' onClick={() => ({})}>Log In</button>
-        <p className='footnote'>By clicking you have agreed to our terms and<br /> conditions</p>
-      </form>
-    </div >
   )
 }
 
