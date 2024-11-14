c1d56fe0a1b2187c53fc9dd00f52e22010f0bf10
Saro <sarobaridoo@gmail.com>
2024-11-14 03:46:44 +0100

    improve the App.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Homepage/Movies/movies';
 import Landing from './landing_page/app';
 import Signup from './landing_page/Hero/signup';
 import './App.css';
 
 const App = () => {
+  const [isGuest, setIsGuest] = useState(false);
+
+  const handleGuestAccess = () => {
+    setIsGuest(true); // Allow guest access
+  };
+
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route path="/movies" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route 
+          path="/movies" 
+          element={isGuest ? <Home /> : <Navigate to="/signup" />} 
+        />
+        <Route 
+          path="/signup" 
+          element={<Signup onGuestAccess={handleGuestAccess} />} 
+        />
       </Routes>
     </Router>
   );
